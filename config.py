7a1443de8cefae3083f3e62dd80b654a94d0b091
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2023-09-30 19:02:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,19 +26,19 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "20960841")
+    API_HASH  = os.environ.get("API_HASH", "a66cd145b429e740d0d93ae3375658ec")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6119647646:AAF6a1ltfTNw7l8e6sg9M3jVwx5PhQ5RWwk") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://Bastin:Bastin@cluster0.irj81sj.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/7e154620a0984ecc1e6a2.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "CM_HUB_CHANNEL") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
 
     # wes response configuration     
